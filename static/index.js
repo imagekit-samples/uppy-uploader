ac7f886cb4a4646b189b778a18a3c1d6a2cb00cb
Manu Chaudhary <manu@imagekit.io>
2020-07-18 19:05:32 +0530

    variable change

--- static/index.js
+++ static/index.js
@@ -8,17 +8,19 @@ import GoogleDrive from '@uppy/google-drive'
 import Dropbox from '@uppy/dropbox'
 import Facebook from '@uppy/facebook'
 
+const SERVER_URL = "https://593487260f15.ngrok.io";
+const IMAGEKIT_PUBLIC_KEY = "public_BimyddYm83A5tzSLpdF71jNfgoI=";
 
 const uppy = Uppy({ debug: true, autoProceed: false })
     .use(Dashboard, { inline: true, trigger: '#uppyDashboard' })
-    .use(GoogleDrive, { target: Dashboard, companionUrl: 'https://593487260f15.ngrok.io' }) // don't had trailing slash
-    .use(Dropbox, { target: Dashboard, companionUrl: 'https://593487260f15.ngrok.io'})
-    .use(Facebook, { target: Dashboard, companionUrl: 'https://593487260f15.ngrok.io'})
-    .use(Url, { target: Dashboard, companionUrl: 'https://593487260f15.ngrok.io' })
+    .use(GoogleDrive, { target: Dashboard, companionUrl: SERVER_URL }) // don't had trailing slash
+    .use(Dropbox, { target: Dashboard, companionUrl: SERVER_URL})
+    .use(Facebook, { target: Dashboard, companionUrl: SERVER_URL})
+    .use(Url, { target: Dashboard, companionUrl: SERVER_URL })
     .use(ImageKitUppyPlugin, {
         id: 'ImageKit',
-        authenticationEndpoint: 'https://593487260f15.ngrok.io/auth',
-        publicKey: "public_BimyddYm83A5tzSLpdF71jNfgoI=",
+        authenticationEndpoint: `${SERVER_URL}/auth`,
+        publicKey: IMAGEKIT_PUBLIC_KEY,
         metaFields: ["useUniqueFileName", "tags", "folder", "isPrivateFile", "customCoordinates", "responseFields"]
     })
 
